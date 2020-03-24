c2c7fb1c78d75904da6da4f630ebc7457e561591
Davide Morgado <davidemorgado@ua.pt>
2020-03-24 23:28:24 +0000

    File TrackCambada

--- Entrega3/TrackCambada.py
+++ Entrega3/TrackCambada.py
@@ -1,35 +1,46 @@
 import numpy as np
-import cv2 as cv
-drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-ix,iy = -1,-1
-# mouse callback function
-def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing,mode
-    if event == cv.EVENT_LBUTTONDOWN:
-        drawing = True
-        ix,iy = x,y
-    elif event == cv.EVENT_MOUSEMOVE:
-        if drawing == True:
-            if mode == True:
-                cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-            else:
-                cv.circle(img,(x,y),5,(0,0,255),-1)
-    elif event == cv.EVENT_LBUTTONUP:
-        drawing = False
-        if mode == True:
-            cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-        else:
-            cv.circle(img,(x,y),5,(0,0,255),-1)
-            
-img = cv.VideoCapture('trackCambada.mp4')
-cv.namedWindow('image')
-cv.setMouseCallback('image',draw_circle)
-while(1):
-    cv.imshow('image',img)
-    k = cv.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
-    elif k == 27:
+import cv2
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture('cambada.mp4')
+
+while(True):
+
+    # Make a window for the video feed  
+    cv2.namedWindow('frame',0)
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Make the trackbar used for HSV masking    
+    cv2.createTrackbar('HSV','frame',0,255,nothing)
+
+    # Name the variable used for mask bounds
+    j = cv2.getTrackbarPos('HSV','frame')
+
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of color in HSV
+    lower = np.array([j-10,-100,-100])
+    upper = np.array([j+10,255,255])
+
+    # Threshold the HSV image to get only selected color
+    mask = cv2.inRange(hsv, lower, upper)
+
+    # Bitwise-AND mask the original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    # Display the resulting frame
+    cv2.imshow('frame',res)
+
+    # Press q to quit
+    if cv2.waitKey(3) & 0xFF == ord('q'):
         break
-cv.destroyAllWindows()
+
+
+# When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows()
