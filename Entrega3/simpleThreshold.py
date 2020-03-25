2468e48620ebf02c5823daf14fab64c676a1d200
Davide Morgado <davidemorgado@ua.pt>
2020-03-25 01:49:16 +0000

    SimpreThreshold new version

--- Entrega3/simpleThreshold.py
+++ Entrega3/simpleThreshold.py
@@ -2,8 +2,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-img = cv.imread('Cores2.png')
+img = cv.imread('CambadaIMAGE.png')
 
 ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
 ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
