af13f12a24139cd13932f163187f09ebfb3821e9
Davide Morgado <davidemorgado@ua.pt>
2020-04-22 12:20:12 +0100

    Good code for 4 task

--- Entrega4/ponto4.py
+++ Entrega4/ponto4.py
@@ -2,20 +2,25 @@ import cv2
 import numpy as np
 
 img = cv2.imread('2.png',0)
-img = cv2.medianBlur(img,1)
-cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+gray_img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)	# Convert to gray-scale
 
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,40	,40,
-                            param1=10,param2=10,minRadius=0,maxRadius=0)  # image, dp, minDistance, minRadious and maxRadious
+blured = cv2.medianBlur(img,3)					# Blur the image to reduce noise
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),1,(0,0,255),3)
 
-cv2.imshow('detected circles',cimg)
+circles = cv2.HoughCircles(blured,cv2.HOUGH_GRADIENT,1	, img.shape[0]/64,
+                            param1=300,param2=40,minRadius=0,maxRadius=0)  # image, dp, minDistance, minRadious and maxRadious
+
+
+if circles is not None:
+	circles = np.uint16(np.around(circles))
+	for i in circles[0, :]:
+		# draw the outer circle
+		cv2.circle(gray_img,(i[0],i[1]),i[2],(0,255,0),2)
+		# draw the center of the circle
+		
+		cv2.circle(gray_img,(i[0],i[1]),1,(0,0,255),3)
+
+cv2.imshow('detected circles',gray_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
